dc06c3ff32b2bb401e699d7ba75a49c65da5f534
Chuks <63956312+skido600@users.noreply.github.com>
2025-01-16 20:49:55 +0100

    helped

--- src/components/Signin.tsx
+++ src/components/Signin.tsx
@@ -47,16 +47,13 @@ const Signin: React.FC = () => {
     setSuccess(false); // Reset success state
 
     try {
-      const response = await fetch(
-        "http://localhost:3000/api/customAuth/register",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ name, email, password }),
-        }
-      );
+      const response = await fetch("/api/customAuth/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name, email, password }),
+      });
 
       const data = await response.json();
 
